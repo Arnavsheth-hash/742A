92127fead930bb4d428ae13bf4721a3f7d06ef50
Arnav Sheth <arnavsheth.programming@gmail.com>
2024-08-17 15:48:17 -0700

    nothing rlly

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,44 @@
 #include "main.h"
+#include "lemlib/api.hpp"
+#include "lemlib/chassis/trackingWheel.hpp"
+#include "pros/misc.h"
+#include "pros/motors.h"
+
+
+ /**
+	* Motor Declarations
+ */
+pros::MotorGroup left_motor_group({ 1, 2, -3 }, pros::MotorGearset::blue); // Numbers are port numbers, negative is reversed
+pros::MotorGroup right_motor_group({ 4, 5, -6 }, pros::MotorGearset::blue); // Numbers are port numbers, negative is reversed
+
+
 
 /**
- * A callback function for LLEMU's center button.
- *
- * When this callback is fired, it will toggle line 2 of the LCD text between
- * "I was pressed!" and nothing.
- */
+	* Drivetrain Settings
+*/
+lemlib::Drivetrain drivetrain(&left_motor_group, // left motor group
+							  &right_motor_group, // right motor group
+							  11.25, // 11.25 inch track width
+							  lemlib::Omniwheel::NEW_275, // 2.75 inch wheel diameter
+							  450, // 450 rpm drive speed
+							  2 // horizontal drive (CHANGE LATER)
+							  );
+
+/**
+	* IMU and Tracking Declarations
+
+	* Can use either rotation sensor or paired encoder ports ('A', 'B') - Check LemLib Documentation fore more info
+*/
+pros::Imu imu(5); // Port 5
+pros::Rotation vertical_sensor(15); // Port 15 and Reversed
+lemlib::TrackingWheel vertical_tracking_wheel(&vertical_sensor, lemlib::Omniwheel::NEW_275, -0.5);
+
+lemlib::OdomSensors sensors(&vertical_tracking_wheel, // vertical tracking wheel 1
+							nullptr, // vertical tracking wheel 2, that does not exist - set to null
+							nullptr, // horizontal tracking wheel 1, that does not exist - set to null
+							nullptr, // horizontal tracking wheel 2, that does not exist - set to null
+							&imu // inertial sensor
+);
 void on_center_button() {
 	static bool pressed = false;
 	pressed = !pressed;
@@ -16,6 +49,45 @@ void on_center_button() {
 	}
 }
 
+
+/**
+	* PID Constants
+*/
+
+// lateral PID controller
+lemlib::ControllerSettings lateral_controller(6, // proportional gain (kP)
+                                              0, // integral gain (kI)
+                                              4.5, // derivative gain (kD)
+                                              0, // anti windup
+                                              1, // small error range, in inches
+                                              100, // small error range timeout, in milliseconds
+                                              3, // large error range, in inches
+                                              500, // large error range timeout, in milliseconds
+                                              0 // maximum acceleration (slew)
+);
+
+// angular PID controller
+lemlib::ControllerSettings angular_controller(4.75, // proportional gain (kP)
+                                              0, // integral gain (kI) - not used
+                                              3.5, // derivative gain (kD)
+                                              0, // anti windup - not used
+                                              3, // small error range, in degrees
+                                              100, // small error range timeout, in milliseconds
+                                              15, // large error range, in degrees
+                                              500, // large error range timeout, in milliseconds
+                                              0 // maximum acceleration (slew) - not used
+);
+
+/**
+	* Chassis Constructor
+*/
+
+lemlib::Chassis chassis(drivetrain, // drivetrain settings
+						lateral_controller, // lateral PID settings
+						angular_controller, // angular PID settings
+						sensors // odometry settings
+					    );
+
 /**
  * Runs initialization code. This occurs as soon as the program is started.
  *
@@ -23,10 +95,19 @@ void on_center_button() {
  * to keep execution time for this mode under a few seconds.
  */
 void initialize() {
-	pros::lcd::initialize();
-	pros::lcd::set_text(1, "Hello PROS User!");
-
-	pros::lcd::register_btn1_cb(on_center_button);
+    pros::lcd::initialize(); // initialize brain screen
+    chassis.calibrate(); // calibrate sensors
+    // print position to brain screen
+    pros::Task screen_task([&]() {
+        while (true) {
+            // print robot location to the brain screen
+            pros::lcd::print(0, "X: %f", chassis.getPose().x); // x
+            pros::lcd::print(1, "Y: %f", chassis.getPose().y); // y
+            pros::lcd::print(2, "Theta: %f", chassis.getPose().theta); // heading
+            // delay to save resources
+            pros::delay(20);
+        }
+    });
 }
 
 /**
@@ -58,7 +139,21 @@ void competition_initialize() {}
  * will be stopped. Re-enabling the robot will restart the task, not re-start it
  * from where it left off.
  */
-void autonomous() {}
+void autonomous() {
+	// set pos to x: 0, y: 0, theta: 0
+	chassis.setPose(0,0,0);
+	// turn to face 90 with very long timeout
+	chassis.turnToHeading(-90, 4000);
+	chassis.moveToPoint(-24,0, 4000);
+	chassis.turnToHeading(0, 4000);
+	chassis.moveToPoint(-24, 24, 4000);
+	chassis.turnToHeading(90, 4000);
+	chassis.moveToPoint(0, 24, 4000);
+	chassis.turnToHeading(180, 4000);
+	chassis.moveToPoint(0, 0, 4000);
+	chassis.turnToHeading(0, 4000);
+
+}
 
 /**
  * Runs the operator control code. This function will be started in its own task
@@ -73,22 +168,55 @@ void autonomous() {}
  * operator control task will be stopped. Re-enabling the robot will restart the
  * task, not resume it from where it left off.
  */
+
+
+pros::Controller controller(pros::E_CONTROLLER_MASTER);
+
+// Button Declarations
+bool intakeButton = false;
+bool outtakeButton = false;
+
+// PTO Declarations
+bool PTOButton = false;
+
+// Mogo Declarations
+bool mogoToggle = false;
+
+// Macro Declarations
+bool armUp = false;
+
+bool armDown = false;
+
+
 void opcontrol() {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::MotorGroup left_mg({1, -2, 3});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
-	pros::MotorGroup right_mg({-4, 5, -6});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
+    // loop forever
+    while (true) {
+		/* Button Assignments */
+		intakeButton = controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1);
+		outtakeButton = controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2);
+		mogoToggle = controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_L1);
 
+		if (intakeButton) {
+			// intake
+		}
+		if (outtakeButton) {
+			// outtake
+		}
 
-	while (true) {
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);  // Prints status of the emulated screen LCDs
+		if (mogoToggle) {
+			// do something
+		}
 
-		// Arcade control scheme
-		int dir = master.get_analog(ANALOG_LEFT_Y);    // Gets amount forward/backward from left joystick
-		int turn = master.get_analog(ANALOG_RIGHT_X);  // Gets the turn left/right from right joystick
-		left_mg.move(dir - turn);                      // Sets left motor voltage
-		right_mg.move(dir + turn);                     // Sets right motor voltage
-		pros::delay(20);                               // Run for 20 ms then update
-	}
+		/* Controller Movements */
+        // get left y and right x positions
+        int leftY = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+        int rightX = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
+
+        // move the robot
+        chassis.arcade(leftY, rightX, false, 0.5);
+
+        // delay to save resources
+        pros::delay(25);
+
+    }
 }
